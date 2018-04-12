ccb3155af4f7ee9b4bdcb86a585273df040cf546
Alek Armbruster <alekarmbruster@gmail.com>
2018-04-12 10:47:57 +0200

    buildNetwork.sh script done

---
+++ test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+MODE=$1;shift
+
+#Create the network using docker compose
+if [ "${MODE}" == "up" ]; then
+  generateCerts
+fi
+
+function generateCerts (){
+  which cryptogen
+  if [ "$?" -ne 0 ]; then
+    echo "cryptogen tool not found. exiting"
+    exit 1
+  fi
+  echo
+  echo "##########################################################"
+  echo "##### Generate certificates using cryptogen tool #########"
+  echo "##########################################################"
+
+  if [ -d "crypto-config" ]; then
+    rm -Rf crypto-config
+  fi
+  set -x
+  cryptogen generate --config=./crypto-config.yaml
+  res=$?
+  set +x
+  if [ $res -ne 0 ]; then
+    echo "Failed to generate certificates..."
+    exit 1
+  fi
+  echo
+}
